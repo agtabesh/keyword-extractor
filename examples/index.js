08bd9db2189b8bfbd7668ba6c25555d58f063ea2
Ahmad <ifireir@gmail.com>
2019-03-15 21:07:54 +0330

    test: refactor test code

--- examples/index.js
+++ examples/index.js
@@ -1,5 +1,10 @@
 const { EnglishTokenizer, KeywordExtractor } = require('../src')
-const documents = require('./data')
+
+const documents = [
+  'Austria will press ahead with a proposed tax on internet giants after plans for an European Union-wide levy fell through this week, Finance Minister Hartwig Loeger said on Friday.',
+  'Facebook Inc said on Friday it would use artificial intelligence to combat the spread of intimate photos shared without people\'s permission, sometimes called "revenge porn," on its social networks.',
+  'Tesla Inc unveiled its Model Y electric sports utility vehicle on Thursday evening in California, promising a much-awaited crossover that will face competition from European car makers rolling out their own electric rivals.'
+]
 
 const tokenizer = new EnglishTokenizer()
 const keywordExtractor = new KeywordExtractor()
